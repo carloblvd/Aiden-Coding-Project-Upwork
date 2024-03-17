38f3a0b94fd678bbef9826ec2b71bc5501f63815
carloblvd <carloblvd@gmail.com>
2024-03-17 10:51:13 -0400

    contact page complete

--- index.js
+++ index.js
@@ -17,34 +17,26 @@ function loadContent(page) {
 }
 
 function submitForm(event) {
-  // Prevent the default form submission behavior
   event.preventDefault();
 
-  // Retrieve the input field values
-  var name = document.querySelector(".contact__name").value;
-  var email = document.querySelector(".contact__email").value;
-  var message = document.querySelector(".contact__form textarea").value;
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+  const messageInput = document.getElementById("message");
 
-  // Validate input values
-  if (name.trim() === "" || email.trim() === "" || message.trim() === "") {
+  const nameValue = nameInput.value.trim();
+  const emailValue = emailInput.value.trim();
+  const messageValue = messageInput.value.trim();
+
+  if (nameValue === "" || emailValue === "" || messageValue === "") {
     alert("Please fill in all fields.");
-    return; // Exit function if any field is empty
+    return;
   }
 
-  // Validate email format
-  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(emailValue)) {
     alert("Please enter a valid email address.");
-    return; // Exit function if email is invalid
+    return;
   }
 
-  // If all validations pass, submit the form
-  document.forms["myContact"].submit();
-
-  // Additional logic for form submission
-  // For example, you might want to send the form data to a server
-
-  // // Implement code to handle form submission here
-  // document.getElementById("contactForm").style.display = "none"; // Hide the form
-  // document.getElementById("thankYou").style.display = "block"; // Show the thank you message
+  event.target.submit();
 }
